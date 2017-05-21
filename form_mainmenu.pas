b0d554788893502328d657fc6cc40ea57923993e
GoldGem <mishachemo@gmail.com>
2017-05-21 20:03:39 +0300

    2
    2

--- form_mainmenu.pas
+++ form_mainmenu.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Grids,
-  ExtCtrls, Menus;
+  ExtCtrls, Menus, LCLType;
 
 type
 
@@ -17,7 +17,12 @@ type
     GameModeBtn: TMenuItem;
     ExitBtn: TMenuItem;
     NewGameBtn: TMenuItem;
-    StringGrid1: TStringGrid;
+    StringGridMain: TStringGrid;
+    procedure ExitBtnClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure StringGridMainKeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
+
   private
     { private declarations }
   public
@@ -27,9 +32,49 @@ type
 var
   FormMainMenu: TFormMainMenu;
 
+
 implementation
 
 {$R *.lfm}
+
+{ TFormMainMenu }
+
+procedure TFormMainMenu.FormCreate(Sender: TObject);
+var
+   i,j:integer;
+begin
+   randomize;
+   for i:=0 to 3 do
+       for j:=0 to 3 do
+           StringGridMain.Cells[i,j]:='0';
+   i:=random(3);
+   j:=random(3);
+   StringGridMain.Cells[i,j]:='2';
+
+end;
+
+procedure TFormMainMenu.StringGridMainKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+var
+   i,j:integer;
+begin
+  if key = VK_RIGHT then
+  for i:=0 to 3 do
+      for j:=0 to 3 do
+          if StringGridMain.Cells[i,j]<>'0' then
+          begin
+             StringGridMain.Cells[3,j]:='2';
+             //if i<>4 then
+               // StringGridMain.Cells[i,j]:='0';
+          end;
+end;
+
+
+
+procedure TFormMainMenu.ExitBtnClick(Sender: TObject);
+begin
+  Close;
+end;
 
 end.
 
